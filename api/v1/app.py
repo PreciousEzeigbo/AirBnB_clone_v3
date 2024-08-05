86b4443b7582f7211f0b1fdc2d23cb84f1442d6d
PreciousEzeigbo <preciousezeigbo@gmail.com>
2024-08-05 06:32:17 -0700

    Fixing errors hopefully

--- api/v1/app.py
+++ api/v1/app.py
@@ -41,8 +41,6 @@ def error_400(error):
 
 
 if __name__ == '__main__':
-    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
     app.run(
         host=app_host,
         port=app_port,
