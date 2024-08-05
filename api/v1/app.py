6beea0438f006877daea7e2174780d7d03c1a91c
Precious Ezeigbo <preciousezeigbo81@gmail.com>
2024-08-05 16:50:05 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,6 +16,8 @@ app_port = int(os.getenv('HBNB_API_PORT', '5000'))
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 CORS(app, resources={'/*': {'origins': app_host}})
+storage = DBStorage()
+storage.reload()
 
 
 @app.teardown_appcontext
